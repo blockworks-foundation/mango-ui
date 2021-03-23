66761980a4b78310c09c0233c8f42ebdefcb72f6
Tyler Shipe <tjshipe@gmail.com>
2021-03-22 21:36:14 -0400

    update usdt alert copy

--- src/components/BasicLayout.js
+++ src/components/BasicLayout.js
@@ -47,9 +47,9 @@ export default function BasicLayout({ children }) {
           <UsdtAlert>
             <div style={{ marginRight: 'auto' }}></div>
             <span style={{ letterSpacing: 0.5 }}>
-              Serum DEX is transitioning all liquidity pairs from wrapped USDT (wUSDT) to native
-              USDT. <a href="https://wusdt.mango.markets">Click here</a> to access and close your
-              wrapped USDT positions.{' '}
+              Serum is transitioning from wrapped USDT (wUSDT) to native USDT.{' '}
+              <a href="https://wusdt.mango.markets">Click here</a> to access and close your wrapped
+              USDT positions.{' '}
               <a
                 href="https://docs.mango.markets/tutorials/transfer-funds-to-sollet-wallet#how-to-swap-wrapped-usdt-for-native-usdt"
                 target="_blank"
